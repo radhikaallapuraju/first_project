7f808fd4abb1e6d18b53d7fbcd8d74c63896688f
radhikaallapuraju <radhikaallapuraju@gmail.com>
2023-03-07 19:04:04 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def NTR(request):
+    return HttpResponse('<marquee>Ntr fan ekkada........#</marquee>')
